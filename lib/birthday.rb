8fa27a18d67f8c98af26eb11c3b29993bb8f565a
matthew enbar <matthew.enbar@gmail.com>
2020-03-29 01:50:16 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,11 +4,12 @@
 #   "Amanda" => 27
 # }
 
-def happy_birthday(birthday_kids)
-  # add your code snippet here!
+
   def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
 end
+
+def age_appropriate_birthday(birthday_kids)
